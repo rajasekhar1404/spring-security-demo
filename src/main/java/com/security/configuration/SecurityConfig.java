26b9fb1e51beae3d131b11acaf3e3a389cec72bc
rajasekhar1404 <mrrajasekhar09@gmail.com>
2023-01-29 23:29:02 +0530

    spring security demo

--- src/main/java/com/security/configuration/SecurityConfig.java
+++ src/main/java/com/security/configuration/SecurityConfig.java
@@ -1,2 +1,43 @@
-package com.security.configuration;public class SecurityConfig {
+package com.security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests()
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .requestMatchers("/home").hasRole("USER")
+                .requestMatchers("/welcome").permitAll()
+                .anyRequest().authenticated().and()
+                .formLogin()
+                .and()
+                .build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails admin = User.withUsername("Rajasekhar").password(encoder().encode("raja")).roles("ADMIN").build();
+        UserDetails user = User.withUsername("Sekhar").password(encoder().encode("sekhar")).roles("USER").build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
--- src/main/java/com/security/controller/HomeController.java
+++ src/main/java/com/security/controller/HomeController.java
@@ -1,2 +1,24 @@
-package com.security.controller;public class HomeController {
+package com.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/welcome")
+    public String getHome() {
+        return "Welcome to home..!";
+    }
+
+    @GetMapping("/home")
+    public String getMainPage() {
+        return "Welcome User..!";
+    }
+
+    @GetMapping("/admin")
+    public String adminOnly() {
+        return "Hello admin";
+    }
+
 }
